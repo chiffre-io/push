782cc30fbe6b82a3ebab64188a7f129ace66774e
Francois Best <francois@francoisbest.com>
2020-05-10 15:36:25 +0200

    chore: Simpler calculation for next midnight

--- src/utility.ts
+++ src/utility.ts
@@ -5,12 +5,5 @@ export { dayjs }
 dayjs.extend(utc)
 
 export function getNextMidnightUTC(now: number) {
-  return dayjs(now)
-    .utc()
-    .set('h', 0)
-    .set('m', 0)
-    .set('s', 0)
-    .set('ms', 0)
-    .add(1, 'day')
-    .valueOf()
+  return dayjs(now).utc().startOf('day').add(1, 'day').valueOf()
 }
